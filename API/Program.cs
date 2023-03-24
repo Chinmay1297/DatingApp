289827ae95acb4833a48a19dfe220e0c760dbe60
Chinmay Prusty <kanha12297@gmail.com>
2023-03-24 17:22:42 +0530

    Part 2- Walking Skeleton of Clientside App

--- API/Program.cs
+++ API/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors();
+
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -14,6 +16,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();              //Middleware
 
 app.Run();
