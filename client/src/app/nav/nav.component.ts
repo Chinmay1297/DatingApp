dd6289803546eec2c124b193ff95571d750c6213
Chinmay Prusty <kanha12297@gmail.com>
2023-03-26 14:27:11 +0530

    Added nav-links, routing &  authguard.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +14,10 @@ export class NavComponent implements OnInit {
   model:any = {};
   //currentUser$: Observable<User | null> = of(null);
 
-  constructor(public accountService: AccountService){}
+  constructor(
+    public accountService: AccountService, 
+    private router: Router, 
+    private toastr: ToastrService){}
 
   ngOnInit(): void {
    //this.currentUser$ = this.accountService.currentUser$;
@@ -29,12 +34,8 @@ export class NavComponent implements OnInit {
   login()
   {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: err => {
-        console.log(err)
-      },
+      next: () => this.router.navigateByUrl("/members"),
+      error: err => this.toastr.error(err.error),
       complete: ()=>{
         //turn off loader
       }
@@ -44,5 +45,6 @@ export class NavComponent implements OnInit {
   logout()
   {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 }
