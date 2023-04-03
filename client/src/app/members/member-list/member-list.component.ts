25e9024a007f56d682ad1ffa744149f493cfbaa8
Chinmay Prusty <kanha12297@gmail.com>
2023-04-03 20:32:35 +0530

    Added Filter Control for matches.

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { Pagination } from 'src/app/_models/pagination';
+import { UserParams } from '../../_models/userParams';
+import { User } from '../../_models/user';
+import { AccountService } from '../../_services/account.service';
 
 @Component({
   selector: 'app-member-list',
@@ -14,10 +17,21 @@ export class MemberListComponent implements OnInit{
   // members$: Observable<Member[]> | undefined;
   members: Member[] = [];
   pagination: Pagination | undefined;
-  pageNumber = 1;
-  pageSize = 5;
+  userParams: UserParams| undefined;
+  user: User| undefined;
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
 
-  constructor(private memberService: MembersService) {  }
+  constructor(private memberService: MembersService, private accountService: AccountService) { 
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user=>{
+        if(user)
+        {
+          this.user = user;
+          this.userParams = new UserParams(user);
+        }
+      }
+    })
+   }
 
   ngOnInit(): void {
     this.loadMembers();
@@ -26,7 +40,8 @@ export class MemberListComponent implements OnInit{
 
   loadMembers()
   {
-    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+    if(!this.userParams) return;
+    this.memberService.getMembers(this.userParams).subscribe({
         //next: response => this.members = response
         next: response => {
           if(response.result && response.pagination){
@@ -37,11 +52,19 @@ export class MemberListComponent implements OnInit{
     })
   }
 
+  resetFilters(){
+    if(this.user)
+    {
+      this.userParams = new UserParams(this.user);
+      this.loadMembers();
+    }
+  }
+
   pageChanged(event : any)
   {
-    if(this.pageNumber!=event.page)
+    if(this.userParams && this.userParams?.pageNumber!=event.page)
     {
-      this.pageNumber = event.page;
+      this.userParams.pageNumber = event.page;
       this.loadMembers();
     }
   }
